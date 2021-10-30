25947a81ac5d0a647c7c8dca51ed8b8ce2f8b7b0
staner70 <taner2003@gmail.com>
2021-10-30 10:34:43 +0200

    Add database connection

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,7 +4,13 @@ const PokemonModel = require('../models/pokemon');
 const UserModel = require('../models/user');
 const pokemons = require('./mock-pokemon');
 
-const sequelize = new Sequelize('postgres://pokedex:pokedex@localhost:5432/pokedex');
+// const sequelize = new Sequelize('postgres://pokedex:pokedex@localhost:5432/pokedex');
+let sequelize;
+if (process.env.NODE_ENV === 'production') {
+    sequelize = new Sequelize('postgres://vyjyzjtekftezy:22d8a826b74aa68d6524ea8aeb2a1094f2923aaf2b6d80375adbcc0a82ac80f5@ec2-44-198-196-169.compute-1.amazonaws.com:5432/dct63iuojpfh9i');
+} else {
+    sequelize = new Sequelize('postgres://pokedex:pokedex@localhost:5432/pokedex');
+}
 
 const Pokemon = PokemonModel(sequelize, DataTypes);
 const User = UserModel(sequelize, DataTypes);
@@ -15,7 +21,7 @@ sequelize.authenticate()
 
 
 const initDb = () => {
-    return sequelize.sync({force: true}).then(_ => {
+    return sequelize.sync().then(_ => {
         pokemons.map(pokemon => {
         Pokemon.create({
             name: pokemon.name,
